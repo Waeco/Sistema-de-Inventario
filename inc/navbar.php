bc03c58396b4683f119bd762f838be7294a83755
Waeco <juanmanu6609@gmail.com>
2026-06-29 10:34:06 -0600

    primer commit

---
+++ inc/navbar.php
@@ -0,0 +1,82 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+            <img src="./img/inventario_icon.png" alt="" height="112" width="28">
+
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    Usuarios
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a class="navbar-item">
+                        Nuevo
+                    </a>
+                    <a class="navbar-item">
+                        Lista
+                    </a>
+                    <a class="navbar-item">
+                        Buscar
+                    </a>
+                </div>
+                </div>
+                <!--Categorias-->
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a href="" class="navbar-link">Categorias</a>
+                    
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item">
+                            Nueva
+                        </a>
+                        <a class="navbar-item">
+                            Lista
+                        </a>
+                        <a class="navbar-item">
+                            Buscar
+                        </a>
+                    </div>
+                    </div>
+                    <!--Productos-->
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a href="" class="navbar-link">Categorias</a>
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item">
+                                Nuevo
+                            </a>
+                            <a class="navbar-item">
+                                Lista
+                            </a>
+                            <a href="" class="navbar-item">Por categoria</a>
+                            <a class="navbar-item">
+                                Buscar
+                            </a>
+                        </div>
+                    </div>
+                
+            
+        </div>
+
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a href="" class="button is-primary is-rounded">Mi cuenta</a>
+                    <a href="" class="button is-ligh is-rounded">Salir</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/bulma.min.css">
+    <link rel="stylesheet" href="./css/estilos.css">
+    <title>Sistema de inventario</title>
+     
+    
+    <?php include "./inc/head.php" ?>
+</head>
+<body>
+    <?php include("./inc/navbar.php")?>
+    <?php include "./inc/script.php"?>
+</body>
+</html>
