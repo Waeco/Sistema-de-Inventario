689528e66834306ab8d4b4d6a29ef3a856400b9a
Waeco <juanmanu6609@gmail.com>
2026-07-15 23:04:09 -0600

    creacion de funciones para verificar entradas y inyeccion sql

--- php/main.php
+++ php/main.php
@@ -1,7 +1,44 @@
 <?php
 //conexion a la base de datos
 function conexion(){
-    $pdo = new PDO('mysql:host=localhost;dbaname=inventario','root','72fbeb2fg');
+    $conexion = new PDO("mysql:host=localhost;dbname=inventario","root","72fbeb2fg");
+return $conexion;
 }
-
+#verificar datos
+function verificar_datos($filtro,$cadena){
+    if(preg_match("/^".$filtro."$/",$cadena)){
+    return false;
+    }else{
+        return true;
+    }
+}
+function limpiar_cadena($cadena){
+    $cadena = trim($cadena);
+    $cadena = stripslashes($cadena);
+    $cadena = str_ireplace("<script>","",$cadena);
+    $cadena = str_ireplace("</script>","",$cadena);
+    $cadena = str_ireplace("<script src","",$cadena);
+    $cadena = str_ireplace("<script type=","",$cadena);
+    $cadena = str_ireplace("SELECT * FROM","",$cadena);
+    $cadena = str_ireplace("DELETE FROM","",$cadena);
+    $cadena = str_ireplace("INSERT INTO","",$cadena);
+    $cadena = str_ireplace("DROP TABLE","",$cadena);
+    $cadena = str_ireplace("DROP DATABASE","",$cadena);
+    $cadena = str_ireplace("TRUNCATE TABLE","",$cadena);
+    $cadena = str_ireplace("SHOW TABLES;","",$cadena);
+    $cadena = str_ireplace("SHOW DATABASES;","",$cadena);
+    $cadena = str_ireplace("<?php","",$cadena);
+    $cadena = str_ireplace("?>","",$cadena);
+    $cadena = str_ireplace("--","",$cadena);
+    $cadena = str_ireplace(">","",$cadena);
+    $cadena = str_ireplace("<","",$cadena);
+    $cadena = str_ireplace("[","",$cadena);
+    $cadena = str_ireplace("]","",$cadena);
+    $cadena = str_ireplace("^","",$cadena);
+    $cadena = str_ireplace("==","",$cadena);
+    $cadena = str_ireplace(";","",$cadena);
+    $cadena = trim($cadena);
+    $cadena = stripslashes($cadena);
+    return $cadena;
+}
 ?>
